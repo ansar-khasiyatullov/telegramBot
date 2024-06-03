f6506c45b08673ca046ccb9ca9edef1a2c50ef02
intelcore <becar_02@mail.ru>
2024-06-03 06:08:51 +0300

    L20240602

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,21 +3,35 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.model.NotificationTask;
+import pro.sky.telegrambot.repository.NotificationTaskRepository;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private final TelegramBot telegramBot;
+    private final NotificationTaskRepository repository;
 
-    @Autowired
-    private TelegramBot telegramBot;
+    public TelegramBotUpdatesListener(TelegramBot telegramBot, NotificationTaskRepository repository) {
+        this.telegramBot = telegramBot;
+        this.repository = repository;
+    }
+
+    private static final Pattern PATTERN = Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)([\\W+]+)");
 
     @PostConstruct
     public void init() {
@@ -27,10 +41,42 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
+            LOGGER.info("Processing update: {}", update);
             // Process your updates here
+            if (update.message() != null) {
+                String messageText = update.message().text();
+                if (messageText != null) {
+                    if (messageText.equals("/start")) {
+                        SendMessage message = new SendMessage(update.message().chat().id(), "Добро пожаловать");
+                        SendResponse response = telegramBot.execute(message);
+                    } else {
+                        var matcher = PATTERN.matcher(messageText);
+                        if (matcher.matches()) {
+                            var noteTime = parseDate(matcher.group(1));
+                            if (noteTime == null) {
+                                telegramBot.execute(new SendMessage(update.message().chat().id(), "Неправильный формат даты"));
+                                return;
+                            }
+                            var task = new NotificationTask();
+                            task.setNoteText(matcher.group(3));
+                            task.setChatId(update.message().chat().id());
+                            task.setNoteTime(noteTime);
+                            repository.save(task);
+                            LOGGER.info("Task saved: {}", task);
+                        }
+                    }
+                }
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
+    private LocalDateTime parseDate(String date) {
+        try {
+            return LocalDateTime.parse(date, DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
+        } catch (DateTimeParseException e) {
+            LOGGER.error("Incorrect date format: {}", date);
+        }
+        return null;
+    }
 }
